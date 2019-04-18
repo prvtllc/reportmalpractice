ca1254ff8f9eb557dda5cfb0351e0415db84edc8
Domingo Rivera <djr@infosecusa.com>
2019-04-18 17:56:10 -0400

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,7 +10,7 @@ const About = center => (
     <Header title="About Page">Report Malpractice</Header>
     <Container center={center}>
       <h3>
-        The purpose of this website is to create an environment of fairness, transparency and accountability for medical patients and to rais awareness of the behavior of those entrusted with our health. 
+        The purpose of this website is to create an environment of fairness, transparency and accountability for medical patients and to raise awareness of the behavior of those entrusted with our health. 
 
         All reports are from public records or user submissions.  User submissions are screened to reasonably prevent personal attacks or information of little value and credibility.
         read the guide{' '}
